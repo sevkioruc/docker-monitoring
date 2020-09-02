fd5d4e503b4c854f12b1a0efd3c9c793459dd87f
sevkioruc34@gmail.com <sevkioruc34@gmail.com>
2020-09-02 23:59:13 +0300

    Create basic server

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+
+
+app.get('/api/get', (req, res, next) => {
+  res.status(200).send('Dummy Text');
+});
+
+module.exports = app;
