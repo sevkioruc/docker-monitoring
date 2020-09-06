91065f1552577d0cb27590a2f070b353acffb2a4
sevkioruc <sevkioruc34@gmail.com>
2020-09-06 08:07:19 -0700

    container remove feature added

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exec = require('child_process').exec;
-const {getAllContainersAsJSON} = require('./utils/util');
+
+const {getAllContainersAsJSON, removeContainerWithID} = require('./utils/util');
+
 const app = express();
 
 const FORMAT="{{.ID}}:{{.Image}}:{{.Command}}:{{.RunningFor}}:{{.Status}}:{{.Names}}:{{.Ports}}:"
@@ -21,6 +23,12 @@ app.get('/api/get', (req, res) => {
   exec(`docker ps -a --format ${FORMAT}`, 'utf8', (err, stdout) => {
     const containerArray = getAllContainersAsJSON(stdout);
     res.status(200).json(containerArray);
+  })
+});
+
+app.get('/api/remove/:containerID', (req, res) => {
+  exec(`docker container rm ${req.params.containerID}` , 'utf8', (err, stdout) => {
+    res.status(200).json({containerID: stdout});
   })
 });
 
